27c94b34a2c8ace9b4e9ceb8e43146b0dcb2d16a
japsermalik <jaspermalik729@gmail.com>
2019-09-22 23:41:00 +0800

    components

--- src/index.js
+++ src/index.js
@@ -21,9 +21,42 @@ class Header extends Component {
     return (
       <div>
         <Title />
+        <h2>This is Header</h2>
       </div>
     )
   }
 }
 
-ReactDOM.render(<Header />, document.getElementById('root'))
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is main content</h2>
+      </div>
+    )
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is footer</h2>
+      </div>
+    )
+  }
+}
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'))
