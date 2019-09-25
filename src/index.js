f3c031f5ed7e639c0f590862ff612bb9a31df9f2
japsermalik <jaspermalik729@gmail.com>
2019-09-25 22:44:47 +0800

    props.children

--- src/index.js
+++ src/index.js
@@ -10,60 +10,22 @@ import './index.css'
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
-class Clock extends Component {
-  constructor() {
-    super()
-    this.state = { date: new Date() }
-  }
-  componentWillMount() {
-    this.time = setInterval(() => {
-      this.setState({ date: new Date() })
-    }, 1000)
-  }
-  componentWillUnmount() {
-    clearTimeout(this.time)
-  }
+class Card extends Component {
   render() {
     return (
-      <div>
-        <h1>
-          <p>现在时间是</p>
-          {this.state.date.toLocaleTimeString()}
-        </h1>
+      <div className="card">
+        <div className="card-content">{this.props.children}</div>
       </div>
     )
   }
 }
 
-class AutoFocusInput extends Component {
-  componentDidMount() {
-    this.input.focus()
-  }
-  render() {
-    return <input ref={input => (this.input = input)} />
-  }
-}
-class Index extends Component {
-  constructor() {
-    super()
-    this.state = { isShowClock: true }
-  }
-
-  handleShowOrHide() {
-    this.setState({ isShowClock: !this.state.isShowClock })
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.isShowClock ? <Clock /> : null}
-        <button onClick={this.handleShowOrHide.bind(this)}>
-          显示或隐藏时钟
-        </button>
-        <AutoFocusInput />
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<Index />, document.getElementById('root'))
+ReactDOM.render(
+  <Card>
+    <h2>React.js 小书</h2>
+    <div>开源、免费、专业、简单</div>
+    订阅：
+    <input />
+  </Card>,
+  document.getElementById('root')
+)
