7f3c3998ff9a416a987c0b1688a81b752f14269a
japsermalik <jaspermalik729@gmail.com>
2019-09-27 22:52:22 +0800

    context

--- src/index.js
+++ src/index.js
@@ -1,39 +1,70 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 import './index.css'
-import wrapWithLoadData from './wrapWithLoadData'
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-class InputWithUserName extends Component {
-  render() {
-    return <input value={this.props.data} />
-  }
-}
-
-class TextareaWithContent extends Component {
-  render() {
-    return <textarea value={this.props.data} />
-  }
-}
-
-TextareaWithContent = wrapWithLoadData(TextareaWithContent, 'content')
-
-InputWithUserName = wrapWithLoadData(InputWithUserName, 'username')
-
 class Index extends Component {
+  static childContextTypes = {
+    themeColor: PropTypes.string
+  }
+  constructor() {
+    super()
+    this.state = { themeColor: 'royalblue' }
+  }
+  componentWillMount() {
+    this.setState({ themeColor: 'green' })
+  }
+  getChildContext() {
+    return { themeColor: this.state.themeColor }
+  }
   render() {
     return (
       <div>
-        <InputWithUserName />
-        <TextareaWithContent />
+        <Header />
+        <Main />
+      </div>
+    )
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is Header</h2>
+        <Title />
+      </div>
+    )
+  }
+}
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is main</h2>
+        <Content />
+      </div>
+    )
+  }
+}
+
+class Title extends Component {
+  static contextTypes = {
+    themeColor: PropTypes.string
+  }
+  render() {
+    return <h1 style={{ color: this.context.themeColor }}>React.js 小书标题</h1>
+  }
+}
+
+class Content extends Component {
+  static contextTypes = {
+    themeColor: PropTypes.string
+  }
+  render() {
+    return (
+      <div>
+        <h2 style={{ color: this.context.themeColor }}>React.js小书内容</h2>
       </div>
     )
   }
