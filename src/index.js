31a5bdb9e185c3003a634aa05c3c5be2ace803ed
japsermalik <jaspermalik729@gmail.com>
2019-09-29 21:59:33 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,70 +1,16 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import Header from './Header'
+import Content from './Content'
 import './index.css'
+
 class Index extends Component {
-  static childContextTypes = {
-    themeColor: PropTypes.string
-  }
-  constructor() {
-    super()
-    this.state = { themeColor: 'royalblue' }
-  }
-  componentWillMount() {
-    this.setState({ themeColor: 'green' })
-  }
-  getChildContext() {
-    return { themeColor: this.state.themeColor }
-  }
   render() {
     return (
       <div>
         <Header />
-        <Main />
-      </div>
-    )
-  }
-}
-
-class Header extends Component {
-  render() {
-    return (
-      <div>
-        <h2>This is Header</h2>
-        <Title />
-      </div>
-    )
-  }
-}
-
-class Main extends Component {
-  render() {
-    return (
-      <div>
-        <h2>This is main</h2>
         <Content />
-      </div>
-    )
-  }
-}
-
-class Title extends Component {
-  static contextTypes = {
-    themeColor: PropTypes.string
-  }
-  render() {
-    return <h1 style={{ color: this.context.themeColor }}>React.js 小书标题</h1>
-  }
-}
-
-class Content extends Component {
-  static contextTypes = {
-    themeColor: PropTypes.string
-  }
-  render() {
-    return (
-      <div>
-        <h2 style={{ color: this.context.themeColor }}>React.js小书内容</h2>
       </div>
     )
   }
