8289aba405eb7d19714df9f73be16cae70adbf8e
japsermalik <jaspermalik729@gmail.com>
2019-10-02 20:48:11 +0800

    connect && mapStateToProps

---
+++ src/react-redux.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export const connet = mapSateToProps => WrappedComponent => {
+  class Connect extends Component {
+    static contextTypes = {
+      store: PropTypes.object
+    }
+    constructor() {
+      super()
+      this.state = { allProps: {} }
+    }
+    componentWillMount() {
+      const { store } = this.context
+      this._updateProps()
+      store.subscribe(() => this._updateProps())
+    }
+    _updateProps() {
+      const { store } = this.context
+      let stateProps = mapSateToProps(store.getState(), this.props)
+      this.setState({
+        allProps: {
+          ...stateProps,
+          ...this.props
+        }
+      })
+    }
+    render() {
+      return <WrappedComponent {...this.state.allProps} />
+    }
+  }
+  return Connect
+}
